89b70885cb001b14bb41742f6810a22a039cf522
Mesbahul Islam <mdislam@cs.helsinki.fi>
2016-02-09 16:59:28 +0200

    Deleted unnecessary

--- Showcase/easyasynchttprequest/src/main/java/org/mesba/easyasynchttprequest/RequestTypes.java
+++
@@ -1,9 +0,0 @@
-package org.mesba.easyasynchttprequest;
-
-/**
- * Created by mis on 11/6/2015.
- */
-public interface RequestTypes {
-    String POST = "POST";
-    String GET = "GET";
-}
--- Showcase/easyasynchttprequest/src/main/java/org/mesba/easyasynchttprequest/RequestTypesEnum.java
+++
@@ -1,7 +0,0 @@
-package org.mesba.easyasynchttprequest;
-
-/**
- * Created by mis on 2/7/2016.
- */
-public enum RequestTypesEnum {
-}
